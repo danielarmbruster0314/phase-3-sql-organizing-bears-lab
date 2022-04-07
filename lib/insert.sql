2ccf4c5948be40992ee749cd74e61a173e7777fe
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-07 15:51:15 -0600

    commit tree e06c4a8cb3708d4c66d65781bb8943e609142d92

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears  VALUES (1, 'Mr. Chocolate', 20, 'M', 'BROWN', 'happy', 1 );
+INSERT INTO bears  VALUES (2, 'Rowdy', 30, 'F','RED', 'SAD', 1);
+INSERT INTO bears  VALUES (3, 'Tabitha', 40, 'F', 'BLUE', 'ANGRY', 1);
+INSERT INTO bears  VALUES (4, 'Sergeant Brown', 50, 'M', 'ORANGE', 'GOOFEY', 1);
+INSERT INTO bears  VALUES (5, 'Melissa', 32, 'F', 'PINK', 'GRUMPY', 1);
+INSERT INTO bears  VALUES (6, 'Grinch', 1, 'M', 'BLUE', 'CRANKEY', 1);
+INSERT INTO bears  VALUES (7, 'Wendy', 19, 'F', 'BLACK', 'NICE', 1);
+INSERT INTO bears  VALUES (8, null, 16, 'M', 'MAROON', 'EMPATHETIC', 1);
+
